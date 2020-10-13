0899d0d67c74d7c7992e056c56f51697bb1ed9ed
Rachel Kline <rach3lkline@gmail.com>
2020-10-13 16:41:01 -0400

    Working Handlebars + js files

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,12 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+    $(".member-name").text(data.firstName);
   });
+
+  $("#log-out").on("click", function(){
+    window.location.replace("/login")
+  })
 });
+
+
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,49 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
+  const firstNameInput = $("input#first-name");
+  const lastNameInput = $("input#last-name");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const locationInput = $("input#location-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      location: locationInput.val().trim()
     };
+    console.log(userData);
 
-    if (!userData.email || !userData.password) {
+    if (!userData.firstName || !userData.lastName || !userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.firstName, userData.lastName, userData.email, userData.password, userData.location);
+    firstNameInput.val("");
+    lastNameInput.val("");
     emailInput.val("");
     passwordInput.val("");
+    locationInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName, lastName, email, password, location) {
     $.post("/api/signup", {
+      firstName: firstName,
+      lastName: lastName,
       email: email,
-      password: password
+      password: password,
+      location: location
     })
       .then(() => {
         window.location.replace("/members");
-        // If there's an error, handle it by throwing up a bootstrap alert
+      //   // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
   }
