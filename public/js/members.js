0a09415401b4edebb2c51a6874341823bffaca53
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-10-16 10:03:01 -0400

    added paralax homepage, changed config.json to js, and included .env password.  Added job history api route create or update function, as well as dashboard job history input fields post command.

--- public/js/members.js
+++ public/js/members.js
@@ -58,6 +58,59 @@ $(document).ready(() => {
   // drop down navbar
   $(".dropdown-trigger").dropdown();
 
+  $('.parallax').parallax();
+
+ 
+  const jobOneTitleInput = $("input#job1-title");
+  const employmentOneDatesInput = $("input#employment1-dates");
+  const employerOneInput = $("input#employer1");
+  const jobOneDutiesInput = $("input#job1-duties");
+  const jobTwoTitleInput = $("input#job2-title");
+  const employmentTwoDatesInput = $("input#employment2-dates");
+  const employerTwoInput = $("input#employer2");
+  const jobTwoDutiesInput = $("input#job2-duties");
+  
+    // When the signup button is clicked, we validate the email and password are not blank
+
+    
+  $("#finishJobBtn").on("click", event => {
+    event.preventDefault();
+    const userData = {
+      jobOneTitle: jobOneTitleInput.val().trim(),
+      employerOne: employerOneInput.val().trim(),
+      employmentOneDates: employmentOneDatesInput.val().trim(),
+      responsibilitiesOne: jobOneDutiesInput.val().trim(),
+      jobTwoTitle: jobTwoTitleInput.val().trim(),
+      employerTwo: employerTwoInput.val().trim(),
+      employmentTwoDates: employmentTwoDatesInput.val().trim(),
+      responsibilitiesTwo: jobTwoDutiesInput.val().trim(),
+    };
+    console.log(userData);
+
+    // If we have an email and password, run the signUpUser function
+    storeJobInput(userData.jobOneTitle, userData.employerOne, userData.employmentOneDates, userData.responsibilitiesOne, userData.jobTwoTitle, userData.employerTwo, userData.employmentTwoDates, userData.responsibilitiesTwo);
+  });
+
+  
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function storeJobInput(jobOneTitle, employerOne, employmentOneDates, responsibilitiesOne, jobTwoTitle, employerTwo, employmentTwoDates, responsibilitiesTwo) {
+    $.post("/api/jobhistory", {
+      jobOneTitle: jobOneTitle,
+      employerOne: employerOne,
+      employmentOneDates: employmentOneDates,
+      responsibilitiesOne: responsibilitiesOne,
+      jobTwoTitle: jobTwoTitle,
+      employerTwo: employerTwo,
+      employmentTwoDates: employmentTwoDates,
+      responsibilitiesTwo: responsibilitiesTwo,
+    })
+      .then(() => {
+        window.location.replace("/members");
+      //   // If there's an error, handle it by throwing up a bootstrap alert
+      })
+  }
+
 });
 
 
